73e9d408d800ed20923cdc5d6110a5b407d22d04
Raby Yuson <rabyyuson@gmail.com>
2024-04-04 23:18:25 -0700

    chore: disable unneeded routes

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -7,28 +7,28 @@ import { UpdateReservationDto } from './dto/update-reservation.dto';
 export class ReservationsController {
   constructor(private readonly reservationService: ReservationService) {}
 
-  @Post()
-  create(@Body() createReservationDto: CreateReservationDto) {
-    return this.reservationService.create(createReservationDto);
-  }
+  // @Post()
+  // create(@Body() createReservationDto: CreateReservationDto) {
+  //   return this.reservationService.create(createReservationDto);
+  // }
 
   @Get()
   findAll() {
     return this.reservationService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reservationService.findOne(id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.reservationService.findOne(id);
+  // }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateReservationDto: UpdateReservationDto) {
     return this.reservationService.update(id, updateReservationDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reservationService.remove(id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.reservationService.remove(id);
+  // }
 }
