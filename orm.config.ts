258a1d98a77badfb1de2e3e17b7042ea627b5aab
Raby Yuson <rabyyuson@gmail.com>
2024-04-05 08:38:28 -0700

    chore: use dotenv for configuration

--- orm.config.ts
+++ orm.config.ts
@@ -1,16 +1,17 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Reservation } from 'src/reservations/entities/reservation.entity';
+import dotenv from "dotenv/config";
 
 export default registerAs(
     'orm.config',
     (): TypeOrmModuleOptions => ({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'test1',
-        password: 'test1',
-        database: 'hotel-reservations',
+        host: process.env.HOST,
+        port: Number(process.env.PORT),
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
         entities: [Reservation],
         synchronize: true,
     })
